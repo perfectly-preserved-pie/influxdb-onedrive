2a6222d5647a87a9e4c9616bf5cf53431d9c91b6
Sahib Bhai <lunarwolf01@gmail.com>
2021-10-13 21:45:03 -0700

    Copy the source directory too

--- influxdb_rclone.sh
+++ influxdb_rclone.sh
@@ -4,7 +4,7 @@ today=`date +%m-%d-%Y`
 # Do a full backup of all influxdb databases to a temp directory with the current date
 influxd backup -portable "/home/weewx/influxdb_backup/$today"
 # Copy this new directory to OneDrive
-rclone copy "/home/weewx/influxdb_backup/$today" remote:influxdb
+rclone copy "/home/weewx/influxdb_backup/$today" remote:influxdb/$today
 # If the local directory becomes more than 1GB in size, delete the contents of the local directory
 # First get the size of the directory
 size=`du -sb "/home/weewx/influxdb_backup/"`

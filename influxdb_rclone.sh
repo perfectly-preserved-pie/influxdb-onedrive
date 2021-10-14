81ecb70bc9df1e3e7d7c2ef0b740cc9d63bcecf2
Sahib Bhai <lunarwolf01@gmail.com>
2021-10-13 21:55:46 -0700

    0b76305579eadffad7640024cec05b8ec1e401ca forgot to remove the filepath in the result

--- influxdb_rclone.sh
+++ influxdb_rclone.sh
@@ -7,7 +7,7 @@ influxd backup -portable "/home/weewx/influxdb_backup/$today"
 rclone copy "/home/weewx/influxdb_backup/$today" remote:influxdb/$today
 # If the local directory becomes more than 1GB in size, delete the contents of the local directory
 # First get the size of the directory
-size=`du -sb "/home/weewx/influxdb_backup/"`
+size=`du -sb "/home/weewx/influxdb_backup/" | awk '{print $1}'`
 # then decide what to do
 if [[ "$size" -gt '1073741824' ]]; then
 	rm -rf "/home/weewx/influxdb_backup/*"

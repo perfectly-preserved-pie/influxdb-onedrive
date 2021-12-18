2e543963a59023cb2ca6daee7931802f2fface5c
perfectly-preserved-pie <lunarwolf01@gmail.com>
2021-12-17 18:59:29 -0800

    Update influxdb_rclone.sh

--- influxdb_rclone.sh
+++ influxdb_rclone.sh
@@ -4,6 +4,6 @@ today=`date +%m-%d-%Y`
 # Do a full backup of all influxdb databases to a temp directory with the current date
 influxd backup -portable "/home/weewx/influxdb_backup/$today"
 # Copy this new directory to OneDrive
-rclone copy "/home/weewx/influxdb_backup/$today" remote:influxdb/$today
+rclone copy "/home/weewx/influxdb_backup/$today" onedrive:influxdb/$today
 # To prevent the disk from filling, delete all backups except the latest 3
 cd /home/weewx/influxdb_backup && ls -tp | grep -v '/$' | tail -n +6 | xargs -d '\n' -r rm --

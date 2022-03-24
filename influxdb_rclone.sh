0d08edc4db73bdf8e77cd16b2d4f993a0b70f006
Sahib Bhai <lunarwolf01@gmail.com>
2022-03-23 22:16:12 -0700

    Force remove folders

--- influxdb_rclone.sh
+++ influxdb_rclone.sh
@@ -6,4 +6,4 @@ influxd backup -portable "/home/weewx/influxdb_backup/$today"
 # Copy this new directory to OneDrive
 rclone copy "/home/weewx/influxdb_backup/$today" onedrive:influxdb/$today
 # To prevent the disk from filling, delete all backups except the latest 3
-cd /home/weewx/influxdb_backup && ls -t | tail -n +4 | xargs -I {} rm {}
+cd /home/weewx/influxdb_backup && ls -t | tail -n +4 | xargs -I {} rm -rfv {}
